997f0c37071bb700617770b32ebec53b43d8c923
Siddhartha Varma <39856034+BRO3886@users.noreply.github.com>
2024-11-22 03:07:00 +0530

    update 2024-11-22T03:07:00

--- push.sh
+++ push.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-# Navigate to the wiki directory
 cd ~/Documents/wiki || exit
-
-# Check if there are changes to commit
 if [[ -n $(git status --porcelain) ]]; then
     make all
 else
