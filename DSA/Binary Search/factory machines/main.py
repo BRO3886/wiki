0b1f7d62ace80d8f96b5f9e4c7f586981a2ff14e
Siddhartha Varma <39856034+BRO3886@users.noreply.github.com>
2024-12-12 22:00:01 +0530

    update 2024-12-12T22:00:01

---
+++ DSA/Binary Search/factory machines/main.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def possible(seconds: int, max_products: int, machine_times: List[int]) -> bool:
+    for mt in machine_times:
+        req = seconds
+        if mt > req:
+            continue
+            max_products -= 1
+    return max_products <= 0
+
+
+if __name__ == "__main__":
+    n, t = list(map(int, input().split(" ")))
+    mt = list(map(int, input().split(" ")))
+
+    low, high = 0, int(1e18)
+    ans = int(1e18)
+
+    ctr = 1
+    while low <= high:
+        mid = (low + high) // 2
+        print(low, high, mid, ctr)
+        if possible(mid, t, mt):
+            ans = min(ans, mid)
+            high = mid - 1
+        else:
+            low = mid + 1
+        ctr += 1
+
+    print(ans)
