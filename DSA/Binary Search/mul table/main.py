5ad686a03032878f2a7b69c403ce9a03f66ad10b
Siddhartha Varma <39856034+BRO3886@users.noreply.github.com>
2024-12-12 22:51:00 +0530

    update 2024-12-12T22:51:00

--- DSA/Binary Search/mul table/main.py
+++ DSA/Binary Search/mul table/main.py
@@ -1,6 +1,24 @@
-if __name__ == "__main__":
-    n = int(input())
+def possible(mid, n):
+    count = 0
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if j * i <= mid:
+                count += 1
 
-    for i in range(n):
-        curr = i + 1
-        
+    return count > (n * n - 1) / 2
+
+
+if __name__ == "__main__":
+
+    n = int(input())
+    low = 0
+    high = n * n
+    ans = n * n
+    while low <= high:
+        mid = (low + high) // 2
+        if possible(mid, n):
+            ans = min(ans, mid)
+            high = mid - 1
+        else:
+            low = mid + 1
+    print(ans)
