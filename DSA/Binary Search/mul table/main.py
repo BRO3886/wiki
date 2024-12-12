6f189334b1d140c530dd2012a05392d8048b5c19
Siddhartha Varma <39856034+BRO3886@users.noreply.github.com>
2024-12-12 22:10:01 +0530

    update 2024-12-12T22:10:00

--- DSA/Binary Search/mul table/main.py
+++ DSA/Binary Search/mul table/main.py
@@ -1,5 +1,11 @@
 
 
+# 1. generate multiplication table?
+# 2. find middle element??
+# above approach seems too sloppy
+# how do I find the middle element without generation?
+# 
+
 if __name__ == "__main__":
     n = int(input())
     
